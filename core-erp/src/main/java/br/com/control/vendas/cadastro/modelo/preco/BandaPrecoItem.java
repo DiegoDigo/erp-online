f87277b3150a26934490912ef4cfbcab8cca3f5c
Diego <di3g0d0ming05@gmail.com>
2017-01-11 16:59:12 -0200

    Carga banda preço e banda preço item

--- core-erp/src/main/java/br/com/control/vendas/cadastro/modelo/preco/BandaPrecoItem.java
+++ core-erp/src/main/java/br/com/control/vendas/cadastro/modelo/preco/BandaPrecoItem.java
@@ -5,49 +5,43 @@ import java.io.Serializable;
 @SuppressWarnings("serial")
 public class BandaPrecoItem implements Serializable {
 	private Long recId;
-	private String codigoTabelaPreco;
-	private Float desconto_maximo;
+	private String codigoTabelaPrecoERP;
+	private Float descontoMaximo;
 	private Integer quantidade;
-	private Long codigoBandaPrecoRecId;
-
+	private String codigoBandaPrecoERP;
 	public Long getRecId() {
 		return recId;
 	}
-
 	public void setRecId(Long recId) {
 		this.recId = recId;
 	}
-
-	public String getCodigoTabelaPreco() {
-		return codigoTabelaPreco;
+	public String getCodigoTabelaPrecoERP() {
+		return codigoTabelaPrecoERP;
 	}
-
-	public void setCodigoTabelaPreco(String codigoTabelaPreco) {
-		this.codigoTabelaPreco = codigoTabelaPreco;
+	public void setCodigoTabelaPrecoERP(String codigoTabelaPrecoERP) {
+		this.codigoTabelaPrecoERP = codigoTabelaPrecoERP;
 	}
-
-	public Float getDesconto_maximo() {
-		return desconto_maximo;
+	
+	public Float getDescontoMaximo() {
+		return descontoMaximo;
 	}
-
-	public void setDesconto_maximo(Float desconto_maximo) {
-		this.desconto_maximo = desconto_maximo;
+	public void setDescontoMaximo(Float descontoMaximo) {
+		this.descontoMaximo = descontoMaximo;
 	}
-
 	public Integer getQuantidade() {
 		return quantidade;
 	}
-
 	public void setQuantidade(Integer quantidade) {
 		this.quantidade = quantidade;
 	}
-
-	public Long getCodigoBandaPrecoRecId() {
-		return codigoBandaPrecoRecId;
+	public String getCodigoBandaPrecoERP() {
+		return codigoBandaPrecoERP;
 	}
-
-	public void setCodigoBandaPrecoRecId(Long codigoBandaPrecoRecId) {
-		this.codigoBandaPrecoRecId = codigoBandaPrecoRecId;
+	public void setCodigoBandaPrecoERP(String codigoBandaPrecoERP) {
+		this.codigoBandaPrecoERP = codigoBandaPrecoERP;
 	}
+	
+	
 
+	
 }
--- repositorio-jdbc-erp/src/main/java/br/com/control/repositorio/mappers/BandaPrecoItemRowMapper.java
+++ repositorio-jdbc-erp/src/main/java/br/com/control/repositorio/mappers/BandaPrecoItemRowMapper.java
@@ -12,11 +12,11 @@ public class BandaPrecoItemRowMapper implements RowMapper<BandaPrecoItem> {
 	@Override
 	public BandaPrecoItem mapRow(ResultSet rs, int rowNum) throws SQLException {
 		BandaPrecoItem banda = new BandaPrecoItem();
-		banda.setCodigoBandaPrecoRecId(rs.getLong("codigo_banda_preco_rec_id"));
-		banda.setCodigoTabelaPreco(rs.getString("codigo_tabela_preco "));
-		banda.setDesconto_maximo(rs.getFloat("desconto_maximo"));
+		banda.setCodigoBandaPrecoERP(rs.getString("CODIGO_BANDA_PRECO_ERP"));
+		banda.setCodigoTabelaPrecoERP(rs.getString("codigo_tabpreco_erp"));
+		banda.setDescontoMaximo(rs.getFloat("desconto_maximo"));
 		banda.setQuantidade(rs.getInt("quantidade"));
-		banda.setRecId(rs.getLong("rec_id"));
+//		banda.setRecId(rs.getLong("rec_id"));
 		return banda;
 	}
 
