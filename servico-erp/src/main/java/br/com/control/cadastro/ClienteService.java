a1d8f45fd9d0a4e89c5dc422ae75b49ebc8cc8a2
Rasa Lariguet <rasa.larig@gmail.com>
2017-02-09 13:54:18 -0200

    ajustes das pastas de logs multi ambiente

--- servico-erp/src/main/java/br/com/control/cadastro/ClienteService.java
+++ servico-erp/src/main/java/br/com/control/cadastro/ClienteService.java
@@ -6,8 +6,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import br.com.control.dao.ClienteDao;
-import br.com.control.portal.integracao.TabelasIntegracao;
-import br.com.control.repositorio.mappers.ClienteRowMapper;
 import br.com.control.vendas.cadastro.modelo.cliente.Cliente;
 
 @Service
