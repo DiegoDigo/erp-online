ca116e714b49c945feae1c7326fe3a4fd6144586
Rasa Lariguet <rasa.larig@gmail.com>
2017-03-21 12:36:01 -0300

    ajustes nos espaços dos endereços utilizando o trim

--- repositorio-jdbc-erp/src/main/java/br/com/control/repositorio/mappers/ClienteEnderecoRowMapper.java
+++ repositorio-jdbc-erp/src/main/java/br/com/control/repositorio/mappers/ClienteEnderecoRowMapper.java
@@ -12,17 +12,17 @@ public class ClienteEnderecoRowMapper implements RowMapper<ClienteEndereco> {
 	@Override
 	public ClienteEndereco mapRow(ResultSet rs, int rowNum) throws SQLException {
 		ClienteEndereco endereco = new ClienteEndereco();
-		endereco.setBairro(rs.getString("bairro"));
-		endereco.setCepEndereco(rs.getString("cep_endereco"));
-		endereco.setCodigoClienteERP(rs.getString("CODIGO_CLIENTE_ERP"));
-		endereco.setEndereco(rs.getString("endereco"));
-		endereco.setMunicipio(rs.getString("municipio"));
+		endereco.setBairro(rs.getString("bairro").trim());
+		endereco.setCepEndereco(rs.getString("cep_endereco").trim());
+		endereco.setCodigoClienteERP(rs.getString("CODIGO_CLIENTE_ERP").trim());
+		endereco.setEndereco(rs.getString("endereco").trim());
+		endereco.setMunicipio(rs.getString("municipio").trim());
 		endereco.setNumero(rs.getInt("numero"));
-		endereco.setOrigemLogradouroERP(rs.getString("ORIGEM_LOGRADOURO_ERP"));
+		endereco.setOrigemLogradouroERP(rs.getString("ORIGEM_LOGRADOURO_ERP").trim());
 		endereco.setPontoReferencia(rs.getString("PONTO_REFERENCIA"));
 //		endereco.setRecId(rs.getLong("rec_id"));
-		endereco.setCodigoTipoEnderecoERP(rs.getString("SIGLA_LOGRADOURO_ERP"));
-		endereco.setUfEndereco(rs.getString("UTF_ENDERECO"));
+		endereco.setCodigoTipoEnderecoERP(rs.getString("SIGLA_LOGRADOURO_ERP").trim());
+		endereco.setUfEndereco(rs.getString("UTF_ENDERECO").trim());
 		return endereco;
 	}
 
