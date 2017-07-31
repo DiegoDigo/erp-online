fec827a9d288182df31f77ec47f43519905ebea5
rasa <rasa.larig@gmail.com>
2017-07-31 10:18:23 -0300

    mudança de nomes e senha dos databases.

---
+++ rest-erp/src/main/java/br/com/control/StartErpOnline.java
@@ -0,0 +1,38 @@
+package br.com.control;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.web.HttpMessageConverters;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
+import org.springframework.context.annotation.Import;
+import org.springframework.http.converter.ByteArrayHttpMessageConverter;
+import org.springframework.jms.annotation.EnableJms;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+import br.com.control.configuracao.ConfiguracaoWeb;
+
+@SpringBootApplication
+@Import({ ConfiguracaoWeb.class, ConexaoBanco.class })
+@EnableAspectJAutoProxy
+@EnableJms
+@EnableScheduling
+public class StartErpOnline extends SpringBootServletInitializer {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StartErpOnline.class, args);
+	}
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(StartErpOnline.class);
+	}
+
+	@Bean
+	public HttpMessageConverters customConverters() {
+		ByteArrayHttpMessageConverter arrayHttpMessageConverter = new ByteArrayHttpMessageConverter();
+		return new HttpMessageConverters(arrayHttpMessageConverter);
+	}
+}
