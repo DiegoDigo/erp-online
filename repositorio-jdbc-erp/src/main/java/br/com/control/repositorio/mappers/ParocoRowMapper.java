874ea9298f0ea89e8caab3baa5f4aa2bff14bb80
Rodrigo Augusto Galante <rodrigo.galante@gmail.com>
2017-10-02 19:02:38 -0300

    Implementação de Sincronismo Paroco

--- repositorio-jdbc-erp/src/main/java/br/com/control/repositorio/mappers/ParocoRowMapper.java
+++ repositorio-jdbc-erp/src/main/java/br/com/control/repositorio/mappers/ParocoRowMapper.java
@@ -18,6 +18,12 @@ public class ParocoRowMapper implements RowMapper<Paroco> {
 		paroco.setCodigoClienteErp(rs.getString("CODIGO_CLIENTE_ERP").trim());
 		paroco.setCodigoEmpresaErp(rs.getString("CODIGO_EMPRESA").trim());
 
+		System.out.println(" --> PAROCO");
+		System.out.println("CODIGO_TABELA_PRECO_ERP: " + rs.getString("CODIGO_TABELA_PRECO_ERP").trim());
+		System.out.println("CODIGO_CLIENTE_ERP: " + rs.getString("CODIGO_CLIENTE_ERP").trim());
+		System.out.println("CODIGO_EMPRESA: " + rs.getString("CODIGO_EMPRESA").trim());
+		System.out.println(" -------------------------------------------------- ");
+
 		return paroco;
 	}
 
