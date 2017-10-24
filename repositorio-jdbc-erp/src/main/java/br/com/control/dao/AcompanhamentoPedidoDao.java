6896a5b5570cfbd5b8f1b2017ac9b0533bd3fece
rasa <rasa.larig@gmail.com>
2017-10-24 10:43:51 -0200

    ajuste no parametro da view que mudou o nome.

--- repositorio-jdbc-erp/src/main/java/br/com/control/dao/AcompanhamentoPedidoDao.java
+++ repositorio-jdbc-erp/src/main/java/br/com/control/dao/AcompanhamentoPedidoDao.java
@@ -15,13 +15,13 @@ import br.com.control.vendas.cadastro.modelo.pedido.acompanhemanto.StatusAcompan
 public class AcompanhamentoPedidoDao extends JdbcDao<StatusAcompanhamentoPedido> {
 	
 	public List<StatusAcompanhamentoPedido> listaAcompanhamentoPedido() {
-		String declare = "DECLARE set bigint @codigoprepedido = 0";
+		String declare = "DECLARE set bigint @codigopedido = 0";
 		getJdbcTemplate().update(declare);
 		return selectViewSemWhere(TabelasIntegracao.STATUS_ACOMPANHAMENTO_PEDIDO, new StatusAcompanhamentoPedidoRowMapper());
 	}
 
-	public StatusAcompanhamentoPedido recuperaAcompanhamentoPedido(String numeroPrePedido) {
-		String declare = "DECLARE set bigint @codigoprepedido = "+numeroPrePedido+";";
+	public StatusAcompanhamentoPedido recuperaAcompanhamentoPedido(String numeroPedido) {
+		String declare = "DECLARE set bigint @codigopedido = "+numeroPedido+";";
 		getJdbcTemplate().update(declare);
 		List<StatusAcompanhamentoPedido> retorno = selectViewSemWhere(TabelasIntegracao.STATUS_ACOMPANHAMENTO_PEDIDO, new StatusAcompanhamentoPedidoRowMapper());
 		if (retorno != null && !retorno.isEmpty()) {
