bc3162eb7f81469bd49d3b47f145cef8d06b77a4
Rodrigo Augusto Galante <rodrigo.galante@gmail.com>
2017-10-27 10:49:37 -0200

    Ajuste na chamada da view por código_cliente

--- repositorio-jdbc-erp/src/main/java/br/com/control/dao/VendedorClienteDao.java
+++ repositorio-jdbc-erp/src/main/java/br/com/control/dao/VendedorClienteDao.java
@@ -12,13 +12,13 @@ import br.com.control.vendas.cadastro.modelo.vendedor.VendedorCliente;
 public class VendedorClienteDao extends JdbcDao<VendedorCliente> {
 
 	public List<VendedorCliente> listar() {
-		String declare = "DECLARE set char(3) @CODIGO_VENDEDOR= '';";
+		String declare = "DECLARE set char(20) @CODIGO_CLIENTE = '';";
 		getJdbcTemplate().update(declare);
 		return selectViewSemWhere(TabelasIntegracao.CADASTRO_VENDEDOR_CLIENTE, new VendedorClienteRowMapper());
 	}
 
 	public List<VendedorCliente> recuperarClientesVendedor(String codigoErp) {
-		String declare = String.format("DECLARE set char(3) @CODIGO_VENDEDOR= '%s';", codigoErp);
+		String declare = String.format("DECLARE set char(20) @CODIGO_CLIENTE= '%s';", codigoErp);
 		getJdbcTemplate().update(declare);
 		return selectViewSemWhere(TabelasIntegracao.CADASTRO_VENDEDOR_CLIENTE, new VendedorClienteRowMapper());
 	}
