ca92f72e2c9303f432f67e1002b1064ac1475074
Rodrigo Augusto Galante <rodrigo.galante@gmail.com>
2017-10-28 10:31:55 -0200

    ajuste na chamada da view passando o @odigo_restricao

--- repositorio-jdbc-erp/src/main/java/br/com/control/dao/RestricaoFinanceiraDao.java
+++ repositorio-jdbc-erp/src/main/java/br/com/control/dao/RestricaoFinanceiraDao.java
@@ -1,10 +1,25 @@
 package br.com.control.dao;
 
+import java.util.List;
+
 import org.springframework.stereotype.Repository;
 
+import br.com.control.portal.integracao.TabelasIntegracao;
+import br.com.control.repositorio.mappers.RestricaoFinanceiraRowMapper;
 import br.com.control.vendas.cadastro.modelo.RestricaoFinanceira;
 
 @Repository
 public class RestricaoFinanceiraDao extends JdbcDao<RestricaoFinanceira> {
 
+	public List<RestricaoFinanceira> listar() {
+		String variavelSql = String.format("DECLARE SET varchar(255) @CODIGO_RESTRICAO = ''");
+		getJdbcTemplate().update(variavelSql);
+		return selectViewSemWhere(TabelasIntegracao.CADASTRO_RESTRICAO_FINANCEIRA, new RestricaoFinanceiraRowMapper());
+	}
+
+	public RestricaoFinanceira buscarPorCodigoErp(String codigoErp) {
+		String variavelSql = String.format("DECLARE SET varchar(255) @CODIGO_RESTRICAO = %s ", codigoErp);
+		getJdbcTemplate().update(variavelSql);
+		return selectViewSingle(TabelasIntegracao.CADASTRO_RESTRICAO_FINANCEIRA, new RestricaoFinanceiraRowMapper());
+	}
 }
