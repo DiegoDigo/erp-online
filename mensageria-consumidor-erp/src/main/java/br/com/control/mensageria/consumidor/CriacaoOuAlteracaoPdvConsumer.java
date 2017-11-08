ca06881225472068158ecfca8e3b058aa3ef1b25
Rodrigo Augusto Galante <rodrigo.galante@gmail.com>
2017-11-08 12:21:25 -0200

    logando o objeto cliente

--- mensageria-consumidor-erp/src/main/java/br/com/control/mensageria/consumidor/CriacaoOuAlteracaoPdvConsumer.java
+++ mensageria-consumidor-erp/src/main/java/br/com/control/mensageria/consumidor/CriacaoOuAlteracaoPdvConsumer.java
@@ -1,7 +1,5 @@
 package br.com.control.mensageria.consumidor;
 
-import java.math.BigDecimal;
-
 import javax.jms.JMSException;
 
 import org.slf4j.Logger;
@@ -19,27 +17,28 @@ public class CriacaoOuAlteracaoPdvConsumer {
 
 	private static Logger log = LoggerFactory.getLogger(CriacaoOuAlteracaoPdvConsumer.class);
 
-	
 	@Autowired
 	private ClienteService clienteService;
-	
+
 	@JmsListener(destination = "${portal_ambiente}_alteracao_dados_cadastrais_contato")
 	public void receiveMessage(final Message<ClienteTO> message) throws JMSException {
 		ClienteTO clienteTO = message.getPayload();
-//		clienteTO.setLatitude(BigDecimal.ZERO);
-//		clienteTO.setLongitude(BigDecimal.ZERO);
-//		clienteTO.setEmail("teste@teste.com");
-//		clienteTO.setRg("432223332");
-//		clienteTO.setPontoReferenciaFaturamento("fat");
-//		clienteTO.setPontoReferenciaEntrega("ent");
-//		clienteTO.setPontoReferenciaCobranca("cob");
-//		clienteTO.setMotivoBloqueio("Mot");
-//		clienteTO.setInscricaoEstadual("IE");
-		
+		// clienteTO.setLatitude(BigDecimal.ZERO);
+		// clienteTO.setLongitude(BigDecimal.ZERO);
+		// clienteTO.setEmail("teste@teste.com");
+		// clienteTO.setRg("432223332");
+		// clienteTO.setPontoReferenciaFaturamento("fat");
+		// clienteTO.setPontoReferenciaEntrega("ent");
+		// clienteTO.setPontoReferenciaCobranca("cob");
+		// clienteTO.setMotivoBloqueio("Mot");
+		// clienteTO.setInscricaoEstadual("IE");
+
 		System.out.println(clienteTO);
-		log.info("### RECEBIDO ALTERACAO DO CONTATO DO CLIENTE " +clienteTO.getRazaoSocial() +" - "+ clienteTO.getCpfCnpj() + " DA FILA DE ALTERACAO DE DADOS CADASTRAIS CONTATO ###");
+		log.info("### RECEBIDO ALTERACAO DO CONTATO DO CLIENTE " + clienteTO.getRazaoSocial() + " - "
+				+ clienteTO.getCpfCnpj() + " DA FILA DE ALTERACAO DE DADOS CADASTRAIS CONTATO ###");
+		log.info("--> clienteTO: " + clienteTO);
 		clienteService.salvarOuAlterar(clienteTO);
-		log.info("--> dados do cliente: " + clienteTO.getRazaoSocial() +" inseridos/alterados no erp");
+		log.info("--> dados do cliente: " + clienteTO.getRazaoSocial() + " inseridos/alterados no erp");
 	}
-	
+
 }
