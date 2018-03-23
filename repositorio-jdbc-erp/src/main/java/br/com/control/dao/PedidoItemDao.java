fed913f94499b2290c99bab71e7560a0054d1cd1
rasa.lariguet <rasa.lariguet@NoteCtrlRasa.controlinformatica.com.br>
2018-03-23 15:57:30 -0300

    teste jenkins

--- repositorio-jdbc-erp/src/main/java/br/com/control/dao/PedidoItemDao.java
+++ repositorio-jdbc-erp/src/main/java/br/com/control/dao/PedidoItemDao.java
@@ -19,7 +19,6 @@ public class PedidoItemDao extends JdbcDao<PedidoItem> {
 	
 	public AcompanhamentoPedidoTO salvarItemPedido(PedidoItemTO pedidoItem, String numeroPrePedido) {
 
-		
 		try {
 			logger.info("--> CHAMANDO PROCEDURE ITEM: "+pedidoItem.getSequenciaItem()+" PARA PRE PEDIDO: "+numeroPrePedido);
 			CallableStatement stmt = preparaChamadaProcedure(ProcedureIntegracao.INSERT_ITEM_PRE_PEDIDO);
