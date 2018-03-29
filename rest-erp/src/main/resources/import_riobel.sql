e9fe0545248a237c07bd6af8a589f273a2743caa
rasa.lariguet <rasa.lariguet@NoteCtrlRasa.controlinformatica.com.br>
2018-03-29 17:15:19 -0300

    criando ambiente riobel

---
+++ rest-erp/src/main/resources/import_riobel.sql
@@ -0,0 +1,8 @@
+insert into usuario(nome, login, sistema, password) values ('ADM riobel','portalvendas', 'PORTAL_VENDAS','@erponlinerestapi2017');
+ 
+insert into perfil(nome) values ('ROLE_USER');
+insert into perfil(nome) values ('ROLE_ADMIN');
+insert into perfil(nome) values ('ROLE_GUEST'); 
+
+insert into perfil_usuario(id_usuario, id_perfil) values (1,1);
+insert into perfil_usuario(id_usuario, id_perfil) values (1,2);
