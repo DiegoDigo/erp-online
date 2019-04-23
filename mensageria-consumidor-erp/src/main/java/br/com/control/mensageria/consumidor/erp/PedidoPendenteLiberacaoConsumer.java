131b2c4108289e73676ae127d249b66d345c4d8c
Diego <di3g0d0ming05@gmail.com>
2019-04-23 16:38:21 -0300

    ajuste na carga do combo de produto

--- mensageria-consumidor-erp/src/main/java/br/com/control/mensageria/consumidor/erp/PedidoPendenteLiberacaoConsumer.java
+++ mensageria-consumidor-erp/src/main/java/br/com/control/mensageria/consumidor/erp/PedidoPendenteLiberacaoConsumer.java
@@ -2,6 +2,7 @@ package br.com.control.mensageria.consumidor.erp;
 
 import javax.jms.JMSException;
 
+import br.com.control.portal.enums.StatusLiberacaoPedido;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
