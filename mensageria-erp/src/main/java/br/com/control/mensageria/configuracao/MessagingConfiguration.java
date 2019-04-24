c40f9415f18b1fefc9a73f7989d7dbfa2c1362fb
Rodrigo Galante <rodrigo.galante@businesson.com.br>
2019-04-24 18:31:09 -0300

    retirada de códigos comentados e removi imports indevidos

--- mensageria-erp/src/main/java/br/com/control/mensageria/configuracao/MessagingConfiguration.java
+++ mensageria-erp/src/main/java/br/com/control/mensageria/configuracao/MessagingConfiguration.java
@@ -1,11 +1,5 @@
 package br.com.control.mensageria.configuracao;
 
-import java.util.Arrays;
-
-import javax.annotation.Resource;
-import javax.jms.ConnectionFactory;
-import javax.jms.Session;
-
 import org.apache.activemq.spring.ActiveMQConnectionFactory;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.annotation.Value;
@@ -18,91 +12,58 @@ import org.springframework.jms.config.JmsListenerContainerFactory;
 import org.springframework.jms.core.JmsTemplate;
 import org.springframework.transaction.PlatformTransactionManager;
 
+import javax.annotation.Resource;
+import javax.jms.ConnectionFactory;
+import java.util.Arrays;
+
 @Configuration
 public class MessagingConfiguration {
 
-	@Value("${fila_portal_erp_online}")
-	public String FILA_PORTAL;
-	
-	@Value("${fila_jcontrol_erp_online}")
-	public String FILA_JCONTROL;
-	
-	@Value("${prefixo_ambiente_fila}")
-	private String ambiente;
+    @Value("${fila_portal_erp_online}")
+    public String FILA_PORTAL;
 
-	@Value("${numero_matricula_empresa}")
-	private String matricula;
-	
-	@Resource
-	private PlatformTransactionManager transactionManager;
-	
-	
+    @Value("${fila_jcontrol_erp_online}")
+    public String FILA_JCONTROL;
 
-//    @Bean
-//    public BrokerService brokerPortal() throws Exception {
-//        final BrokerService broker = new BrokerService();
-//        broker.addConnector(FILA_PORTAL);
-//        broker.setBrokerName("brokerPortal");
-//        broker.setUseJmx(false);
-//        return broker;
-//    }
-//
-//    @Bean
-//    public BrokerService brokerJControl() throws Exception {
-//        final BrokerService broker = new BrokerService();
-//        broker.addConnector(FILA_JCONTROL);
-//        broker.setBrokerName("brokerJControl");
-//        broker.setUseJmx(false);
-//        return broker;
-//    }
+    @Value("${prefixo_ambiente_fila}")
+    private String ambiente;
 
-//    @Bean
-//    public ConnectionFactory jmsConnectionFactoryPortal() {
-//        ActiveMQConnectionFactory connectionFactory = new ActiveMQConnectionFactory();
-//        connectionFactory.setBrokerURL(FILA_PORTAL);
-//        return connectionFactory;
-//    }
-//
-//    @Bean
-//    public QueueConnectionFactory jmsConnectionFactoryJControl() {
-//        ActiveMQConnectionFactory connectionFactory = new ActiveMQConnectionFactory();
-//        connectionFactory.setBrokerURL(FILA_JCONTROL);
-//        return connectionFactory;
-//    }
+    @Value("${numero_matricula_empresa}")
+    private String matricula;
 
-	@Bean
-	@Primary
-	public ActiveMQConnectionFactory connectionFactoryPortal() {
-		ActiveMQConnectionFactory connectionFactory = new ActiveMQConnectionFactory();
-		connectionFactory.setBrokerURL(FILA_PORTAL);
-		connectionFactory.setTrustedPackages(Arrays.asList("br.com.control.portal.mensageria.to",
-				"java.lang", "br.com.control.portal.filter",
-				"java.util", "java.math"));
-		System.setProperty("org.apache.activemq.SERIALIZABLE_PACKAGES", "*");
-		return connectionFactory;
-	}
+    @Resource
+    private PlatformTransactionManager transactionManager;
 
-	@Bean
-	public ActiveMQConnectionFactory connectionFactoryJControl() {
-		ActiveMQConnectionFactory connectionFactory = new ActiveMQConnectionFactory();
-		connectionFactory.setBrokerURL(FILA_JCONTROL);
-		connectionFactory.setTrustedPackages(Arrays.asList("br.com.control.portal.mensageria.to", "java.lang", "br.com.control.portal.filter",
-				"java.util", "java.math", "java.sql", "br.com.control.globobroker.model.envio"));
-		System.setProperty("org.apache.activemq.SERIALIZABLE_PACKAGES", "*");
-		
-		return connectionFactory;
-	}
+    @Bean
+    @Primary
+    public ActiveMQConnectionFactory connectionFactoryPortal() {
+        ActiveMQConnectionFactory connectionFactory = new ActiveMQConnectionFactory();
+        connectionFactory.setBrokerURL(FILA_PORTAL);
+        connectionFactory.setTrustedPackages(Arrays.asList("br.com.control.portal.mensageria.to",
+                "java.lang", "br.com.control.portal.filter",
+                "java.util", "java.math"));
+        System.setProperty("org.apache.activemq.SERIALIZABLE_PACKAGES", "*");
+        return connectionFactory;
+    }
 
+    @Bean
+    public ActiveMQConnectionFactory connectionFactoryJControl() {
+        ActiveMQConnectionFactory connectionFactory = new ActiveMQConnectionFactory();
+        connectionFactory.setBrokerURL(FILA_JCONTROL);
+        connectionFactory.setTrustedPackages(Arrays.asList("br.com.control.portal.mensageria.to", "java.lang", "br.com.control.portal.filter",
+                "java.util", "java.math", "java.sql", "br.com.control.globobroker.model.envio"));
+        System.setProperty("org.apache.activemq.SERIALIZABLE_PACKAGES", "*");
 
-
+        return connectionFactory;
+    }
 
     @Bean
     public JmsListenerContainerFactory<?> jmsListenerContainerFactoryPortal(ConnectionFactory connectionFactory,
-    		 DefaultJmsListenerContainerFactoryConfigurer configurer) {
+                                                                            DefaultJmsListenerContainerFactoryConfigurer configurer) {
         DefaultJmsListenerContainerFactory factory = new DefaultJmsListenerContainerFactory();
         configurer.configure(factory, connectionFactory);
         factory.setSessionTransacted(true);
-		factory.setTransactionManager(transactionManager);
+        factory.setTransactionManager(transactionManager);
         return factory;
     }
 
@@ -113,77 +74,79 @@ public class MessagingConfiguration {
         DefaultJmsListenerContainerFactory factory = new DefaultJmsListenerContainerFactory();
         configurer.configure(factory, connectionFactory);
         factory.setSessionTransacted(true);
-		factory.setTransactionManager(transactionManager);
+        factory.setTransactionManager(transactionManager);
         return factory;
     }
 
+    @Bean
+    public JmsTemplate VW_CADASTRO_CLIENTE() {
+        JmsTemplate template = new JmsTemplate();
+        template.setConnectionFactory(connectionFactoryJControl());
+        template.setDefaultDestinationName("VW_CADASTRO_CLIENTE");
+        return template;
+    }
 
-	@Bean
-	public JmsTemplate VW_CADASTRO_CLIENTE() {
-		JmsTemplate template = new JmsTemplate();
-		template.setConnectionFactory(connectionFactoryJControl());
-		template.setDefaultDestinationName("VW_CADASTRO_CLIENTE");
-		return template;
-	}
+    @Bean
+    public JmsTemplate jmsTemplateFilaPedidos() {
+        JmsTemplate template = new JmsTemplate();
+        template.setConnectionFactory(connectionFactoryPortal());
+        template.setDefaultDestinationName(ambiente + "_pedidos_" + matricula);
+        return template;
+    }
 
-	@Bean
-	public JmsTemplate jmsTemplateFilaPedidos() {
-		JmsTemplate template = new JmsTemplate();
-		template.setConnectionFactory(connectionFactoryPortal());
-		template.setDefaultDestinationName(ambiente+"_pedidos_" + matricula);
-		return template;
-	}
+    @Bean
+    public JmsTemplate jmsTemplateAcompanhamentoPedidos() {
+        JmsTemplate template = new JmsTemplate();
+        template.setConnectionFactory(connectionFactoryPortal());
+        template.setDefaultDestinationName(ambiente + "_acompanhamento_" + matricula);
+        return template;
+    }
 
-	@Bean
-	public JmsTemplate jmsTemplateAcompanhamentoPedidos() {
-		JmsTemplate template = new JmsTemplate();
-		template.setConnectionFactory(connectionFactoryPortal());
-		template.setDefaultDestinationName(ambiente+"_acompanhamento_" + matricula);
-		return template;
-	}
+    @Bean
+    public JmsTemplate jmsTemplateSincronismoCadastro() {
+        JmsTemplate template = new JmsTemplate();
+        template.setConnectionFactory(connectionFactoryPortal());
+        template.setDefaultDestinationName(ambiente + "_sincronismo_cadastro_" + matricula);
+        return template;
+    }
 
-	@Bean
-	public JmsTemplate jmsTemplateSincronismoCadastro() {
-		JmsTemplate template = new JmsTemplate();
-		template.setConnectionFactory(connectionFactoryPortal());
-		template.setDefaultDestinationName(ambiente+"_sincronismo_cadastro_" + matricula);
-		return template;
-	}
+    @Bean
+    public JmsTemplate jmsTemplateLiberacaoPedido() {
+        JmsTemplate template = new JmsTemplate();
+        template.setConnectionFactory(connectionFactoryPortal());
+        template.setDefaultDestinationName(ambiente + "_liberacao_pedido_" + matricula);
+        return template;
+    }
 
-	@Bean
-	public JmsTemplate jmsTemplateLiberacaoPedido() {
-		JmsTemplate template = new JmsTemplate();
-		template.setConnectionFactory(connectionFactoryPortal());
-		template.setDefaultDestinationName(ambiente+"_liberacao_pedido_" + matricula);
-		return template;
-	}
-	@Bean
-	public JmsTemplate jmsTemplateAlteracaoDadosCadastraisContato() {
-		JmsTemplate template = new JmsTemplate();
-		template.setConnectionFactory(connectionFactoryPortal());
-		template.setDefaultDestinationName(ambiente+"_alteracao_dados_cadastrais_contato_" + matricula);
-		return template;
-	}
-	@Bean
-	public JmsTemplate jmsTemplateAlteracaoDadosCadastraisEndereco() {
-		JmsTemplate template = new JmsTemplate();
-		template.setConnectionFactory(connectionFactoryPortal());
-		template.setDefaultDestinationName(ambiente+"_alteracao_dados_cadastrais_endereco_" + matricula);
-		return template;
-	}
-	
-	@Bean
-	public JmsTemplate jmsTemplateSincronismoAgendado() {
-		JmsTemplate template = new JmsTemplate();
-		template.setConnectionFactory(connectionFactoryPortal());
-		template.setDefaultDestinationName(ambiente+"_sincronismo_agendado_" + matricula);
-		return template;
-	}
-	@Bean
-	public JmsTemplate jmsTemplatePesquisaSfaMensal() {
-		JmsTemplate template = new JmsTemplate();
-		template.setConnectionFactory(connectionFactoryPortal());
-		template.setDefaultDestinationName(ambiente+"_pesquisa_mensal_sfa_" + matricula);
-		return template;
-	}
+    @Bean
+    public JmsTemplate jmsTemplateAlteracaoDadosCadastraisContato() {
+        JmsTemplate template = new JmsTemplate();
+        template.setConnectionFactory(connectionFactoryPortal());
+        template.setDefaultDestinationName(ambiente + "_alteracao_dados_cadastrais_contato_" + matricula);
+        return template;
+    }
+
+    @Bean
+    public JmsTemplate jmsTemplateAlteracaoDadosCadastraisEndereco() {
+        JmsTemplate template = new JmsTemplate();
+        template.setConnectionFactory(connectionFactoryPortal());
+        template.setDefaultDestinationName(ambiente + "_alteracao_dados_cadastrais_endereco_" + matricula);
+        return template;
+    }
+
+    @Bean
+    public JmsTemplate jmsTemplateSincronismoAgendado() {
+        JmsTemplate template = new JmsTemplate();
+        template.setConnectionFactory(connectionFactoryPortal());
+        template.setDefaultDestinationName(ambiente + "_sincronismo_agendado_" + matricula);
+        return template;
+    }
+
+    @Bean
+    public JmsTemplate jmsTemplatePesquisaSfaMensal() {
+        JmsTemplate template = new JmsTemplate();
+        template.setConnectionFactory(connectionFactoryPortal());
+        template.setDefaultDestinationName(ambiente + "_pesquisa_mensal_sfa_" + matricula);
+        return template;
+    }
 }
