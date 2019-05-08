f75159d6b91f1196e5571f72a1dd84d90c46f82a
Rasa Gargantiel Lariguet <rasa.gargantiel@totvs.com.br>
2019-05-08 12:03:55 -0300

    criarndo regra para saber qual proc chamar.

--- servico-erp/src/main/java/br/com/control/globobroker/MovimentoFinanceiroGloboBrokerService.java
+++ servico-erp/src/main/java/br/com/control/globobroker/MovimentoFinanceiroGloboBrokerService.java
@@ -12,11 +12,19 @@ public class MovimentoFinanceiroGloboBrokerService {
 	@Autowired
 	private MovimentoFinanceiroGloboBrokerDao movimentoFinanceiroDao;
 
-	public String gravaTituloBaixado(MovimentoFinanceiroGloboBroker movimentoFinanceiroGloboBroker) {
-		return movimentoFinanceiroDao.gravaTituloBaixado(movimentoFinanceiroGloboBroker);
+	public void gravarTitulo(MovimentoFinanceiroGloboBroker movimento) {
+		if(movimento.isTituloBaixado()) {
+			movimentoFinanceiroDao.gravaTituloBaixado(movimento);
+		}else if(movimento.isTituloAberto()) {
+			movimentoFinanceiroDao.gravaTituloAberto(movimento);
+		}
 	}
 
-	public String gravaTituloAberto(MovimentoFinanceiroGloboBroker movimentoFinanceiroGloboBroker) {
-		return movimentoFinanceiroDao.gravaTituloAberto(movimentoFinanceiroGloboBroker);
+	public void gravaTituloBaixado(MovimentoFinanceiroGloboBroker movimento) {
+		movimentoFinanceiroDao.gravaTituloBaixado(movimento);
+	}
+
+	public void gravaTituloAberto(MovimentoFinanceiroGloboBroker movimento) {
+		movimentoFinanceiroDao.gravaTituloAberto(movimento);
 	}
 }
