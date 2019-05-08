62fb5c3345b4a3deaebf330838177472e462543a
Rasa Gargantiel Lariguet <rasa.gargantiel@totvs.com.br>
2019-05-08 12:02:30 -0300

    unificando o endpoint e mudando url do globobroker para não ser filtrado pelo tempo de execucao filter.

--- rest-erp/src/main/java/br/com/control/controllers/globobroker/MovimentoFinanceiroGlobalBrokerController.java
+++ rest-erp/src/main/java/br/com/control/controllers/globobroker/MovimentoFinanceiroGlobalBrokerController.java
@@ -2,6 +2,7 @@ package br.com.control.controllers.globobroker;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -10,34 +11,19 @@ import org.springframework.web.bind.annotation.RestController;
 import br.com.control.controllers.AbstractController;
 import br.com.control.globobroker.MovimentoFinanceiroGloboBrokerService;
 import br.com.control.globobroker.model.envio.MovimentoFinanceiroGloboBroker;
-import br.com.control.portal.integracao.MensagemRecebida;
-import br.com.control.portal.integracao.MensagemRetorno;
 import br.com.control.rotas.RotasRest;
 
 @RestController
-@RequestMapping(RotasRest.RAIZ + RotasRest.GLOBO_BROKER + RotasRest.RAIZ_MOVIMENTO + RotasRest.RAIZ_FINANCEIRO)
+@RequestMapping(RotasRest.RAIZ_GLOBO_BROKER +  RotasRest.RAIZ_MOVIMENTO + RotasRest.RAIZ_FINANCEIRO)
 public class MovimentoFinanceiroGlobalBrokerController extends AbstractController {
 
 	@Autowired
 	private MovimentoFinanceiroGloboBrokerService movimentoFinanceiroService;
 
-	@RequestMapping(value = "gravarTituloBaixado", method = RequestMethod.POST, headers = "Accept=application/json")
-	public MensagemRetorno gravarTituloBaixado(
-			@RequestBody MensagemRecebida<MovimentoFinanceiroGloboBroker> movimentoFinanceiroGloboBroker) {
-		String mensagemRetorno = movimentoFinanceiroService
-				.gravaTituloBaixado(movimentoFinanceiroGloboBroker.getConteudo());
-		return new MensagemRetorno(HttpStatus.CREATED, "POST Efetuado!", mensagemRetorno,
-				movimentoFinanceiroGloboBroker.getIdentificacao());
-	}
-
-	@RequestMapping(value = "gravarTituloAberto", method = RequestMethod.POST, headers = "Accept=application/json")
-	public MensagemRetorno gravarTituloAberto(
-			@RequestBody MensagemRecebida<MovimentoFinanceiroGloboBroker> movimentoFinanceiroGloboBroker) {
-		System.out.println("Entrou na controller");
-		String mensagemRetorno = movimentoFinanceiroService
-				.gravaTituloAberto(movimentoFinanceiroGloboBroker.getConteudo());
-		return new MensagemRetorno(HttpStatus.CREATED, "POST Efetuado!", mensagemRetorno,
-				movimentoFinanceiroGloboBroker.getIdentificacao());
+	@RequestMapping(value = "/gravar", method = RequestMethod.POST, headers = "Accept=application/json")
+	public ResponseEntity<String> gravarTituloBaixado(@RequestBody MovimentoFinanceiroGloboBroker movimento) {
+		movimentoFinanceiroService.gravarTitulo(movimento);
+		return new ResponseEntity<String>("TITULOS GRAVADOS COM SUCESSO", HttpStatus.CREATED);
 	}
 
 }
