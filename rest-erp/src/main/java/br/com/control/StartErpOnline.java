97d0a2598e0859eeb1ea5bc24e14c6f7351be43a
Rodrigo <rodrigo.galante@businesson.com.br>
2019-07-01 11:04:11 -0300

    Tratamento para restransmitir pedidos

--- rest-erp/src/main/java/br/com/control/StartErpOnline.java
+++ rest-erp/src/main/java/br/com/control/StartErpOnline.java
@@ -1,10 +1,6 @@
 package br.com.control;
 
-import br.com.control.dao.PedidoCapaDao;
-import br.com.control.portal.Service.PedidoCapaPortalService;
-import br.com.control.portal.mensageria.to.PedidoCapaTO;
-import br.com.control.retransmissao.Serivce.PedidoRetransmissaoService;
-import org.springframework.beans.factory.annotation.Autowired;
+import br.com.control.configuracao.ConfiguracaoWeb;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.web.HttpMessageConverters;
@@ -17,15 +13,10 @@ import org.springframework.core.Ordered;
 import org.springframework.core.annotation.Order;
 import org.springframework.http.converter.ByteArrayHttpMessageConverter;
 import org.springframework.jms.annotation.EnableJms;
-import org.springframework.scheduling.annotation.Async;
 import org.springframework.scheduling.annotation.EnableScheduling;
-
-import br.com.control.configuracao.ConfiguracaoWeb;
-import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 import org.springframework.web.filter.CharacterEncodingFilter;
 
-import java.util.List;
 import java.util.concurrent.Executor;
 
 @SpringBootApplication
@@ -38,9 +29,6 @@ public class StartErpOnline extends SpringBootServletInitializer {
 	public static void main(String[] args) {
 		SpringApplication.run(StartErpOnline.class, args);
 	}
-
-	@Autowired
-	PedidoRetransmissaoService pedidoRetransmissaoService;
 
 	@Override
 	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
@@ -70,25 +58,5 @@ public class StartErpOnline extends SpringBootServletInitializer {
 		executor.setWaitForTasksToCompleteOnShutdown(true);
 		executor.setThreadNamePrefix("erpRetransmissao-");
 		return executor;
-	}
-
-	@Autowired
-	PedidoCapaDao pedidoCapaDao;
-
-	@Async("threadPoolTaskExecutor")
-	@Scheduled(cron = "1 * * * * *")
-	protected void verificarPedidosReTransmissao() {
-		System.out.println("Buscando pedidos não transmitidos");
-		List<PedidoCapaTO> pedidosNaoTransmitidos = pedidoRetransmissaoService.verificar();
-
-
-		pedidosNaoTransmitidos.forEach(pedidoCapa -> {
-			try {
-				pedidoCapaDao.salvarCapaPedido(pedidoCapa);
-				pedidoRetransmissaoService.transmitido(pedidoCapa.getRecId());
-			} catch (Exception e) {
-				System.out.println("Não foi possivel retransmitir o pedido. Causa: " + e.getMessage());
-			}
-		});
 	}
 }
---
+++ rest-erp/src/main/java/br/com/control/components/RetransmissaoService.java
@@ -0,0 +1,51 @@
+package br.com.control.components;
+
+import br.com.control.dao.PedidoCapaDao;
+import br.com.control.mensageria.consumidor.portal.PedidoCapaConsumer;
+import br.com.control.mensageria.produtor.PedidoCapaProducer;
+import br.com.control.portal.mensageria.to.AcompanhamentoPedidoTO;
+import br.com.control.portal.mensageria.to.PedidoCapaTO;
+import br.com.control.portal.mensageria.to.StatusAcompanhamentoPedidoTO;
+import br.com.control.retransmissao.Serivce.PedidoRetransmissaoService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.annotation.Async;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+
+@Component
+public class RetransmissaoService {
+
+    @Autowired
+    PedidoRetransmissaoService pedidoRetransmissaoService;
+
+    @Autowired
+    PedidoCapaProducer producer;
+
+    @Autowired
+    PedidoCapaDao pedidoCapaDao;
+
+    @Async("threadPoolTaskExecutor")
+    @Scheduled(cron = "1 * * * * *")
+    protected void verificarPedidosReTransmissao() {
+        System.out.println("Buscando pedidos não transmitidos");
+        List<PedidoCapaTO> pedidosNaoTransmitidos = pedidoRetransmissaoService.verificar();
+
+        pedidosNaoTransmitidos.forEach(pedidoCapa -> {
+            try {
+                AcompanhamentoPedidoTO acompanhamentoPedidoTO = pedidoCapaDao.salvarCapaPedido(pedidoCapa);
+                pedidoRetransmissaoService.transmitido(pedidoCapa.getRecId());
+                StatusAcompanhamentoPedidoTO status = new StatusAcompanhamentoPedidoTO();
+                status.setNumeroPrePedidoErp(acompanhamentoPedidoTO.getNumeroPedidoGestao());
+                status.setRecId(acompanhamentoPedidoTO.getRecId());
+                System.out.println(("--> status pedido: " + status.recuperaStatus()));
+                System.out.println(("___________________________________________________________"));
+
+                producer.sendMessage(status);
+            } catch (Exception e) {
+                System.out.println("Não foi possivel retransmitir o pedido. Causa: " + e.getMessage());
+            }
+        });
+    }
+}
