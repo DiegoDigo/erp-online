08a8c975985976dc00b2cd9873025a5c112eb6b3
Diego domingos Delmiro - Control Informática <diego.delmiro@controlinformatica.com.br>
2019-09-16 09:22:41 -0300

    ajuste na chamada da view de historico de pedido

--- repositorio-jdbc-erp/src/main/java/br/com/control/dao/HistoricoPedidoCapaDao.java
+++ repositorio-jdbc-erp/src/main/java/br/com/control/dao/HistoricoPedidoCapaDao.java
@@ -15,19 +15,19 @@ import br.com.control.vendas.cadastro.modelo.pedido.HistoricoPedidoCapa;
 public class HistoricoPedidoCapaDao extends JdbcDao<HistoricoPedidoCapa> {
 
 	public List<HistoricoPedidoCapa> listar() {
-		String declare = "DECLARE set varchar(12) @numero_pedido = '' ";
+		String declare = "DECLARE set bigint @numero_pedido = 0 ";
 		getJdbcTemplate().update(declare);
 		return selectViewSemWhere(ViewsIntegracaoERP.VW_HISTORICO_PEDIDO_CAPA, new HistroricoPedidoCapaRowMapper());
 	}
 
 	public HistoricoPedidoCapa buscarHistoricoPedidoCapa(String codigoERP) {
-		String declare = String.format("DECLARE set varchar(12) @numero_pedido = '%s' ", codigoERP);
+		String declare = String.format("DECLARE set bigint @numero_pedido = %f ", Float.parseFloat(codigoERP));
 		getJdbcTemplate().update(declare);
 		return selectViewSingle(ViewsIntegracaoERP.VW_HISTORICO_PEDIDO_CAPA, new HistroricoPedidoCapaRowMapper());
 	}
 	
 	public Integer count() {
-		String declare = "DECLARE set varchar(12) @numero_pedido = '' ";
+		String declare = "DECLARE set bigint @numero_pedido = 0 ";
 		getJdbcTemplate().update(declare);
 		String sql = "select count(*) from "+ getSchemaDatabase() + "." + ViewsIntegracaoERP.VW_HISTORICO_PEDIDO_CAPA;
 		return contaRegistros(sql);
--- repositorio-jdbc-erp/src/main/java/br/com/control/dao/HistoricoPedidoItemDao.java
+++ repositorio-jdbc-erp/src/main/java/br/com/control/dao/HistoricoPedidoItemDao.java
@@ -15,19 +15,19 @@ import br.com.control.vendas.cadastro.modelo.pedido.HistoricoPedidoItem;
 public class HistoricoPedidoItemDao extends JdbcDao<HistoricoPedidoItem> {
 
 	public List<HistoricoPedidoItem> listarPedidoItem() {
-		String declare = "DECLARE set varchar(12) @numero_pedido = '' " ;
+		String declare = "DECLARE set bigint @numero_pedido = 0 " ;
 		getJdbcTemplate().update(declare);	
 		return selectViewSemWhere(ViewsIntegracaoERP.VW_HISTORICO_PEDIDO_ITEM, new HistoricoPedidoItemRowMapper());
 	}
 	
 	public List<HistoricoPedidoItem> listarPedidoItemDaCapa(String numeroPedido) {
-		String declare = String.format("DECLARE set varchar(12) @numero_pedido = '%s' ",numeroPedido) ;
+		String declare = String.format("DECLARE set bigint @numero_pedido = %f ", Float.parseFloat(numeroPedido)) ;
 		getJdbcTemplate().update(declare);	
 		return selectViewSemWhere(ViewsIntegracaoERP.VW_HISTORICO_PEDIDO_ITEM, new HistoricoPedidoItemRowMapper());
 	}
 	
 	public Integer count() {
-		String declare = "DECLARE set varchar(12) @numero_pedido = '' " ;
+		String declare = "DECLARE set bigint @numero_pedido = 0 " ;
 		getJdbcTemplate().update(declare);
 		String sql = "select count(*) from "+ getSchemaDatabase() + "." + ViewsIntegracaoERP.VW_HISTORICO_PEDIDO_ITEM;
 		return contaRegistros(sql);
